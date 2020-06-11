2eb84f6e0fb48e3b235adc236a0310615f6a6507
SHAIK SHABBER ALI <61702403+Shabeer452@users.noreply.github.com>
2020-06-11 17:24:30 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WORKING_AREA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.APARTMENTDETAILSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bILLGENERATEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bILLSTATUSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eNTRYDETAILSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oTHERSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mINUTEBOOKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.APARTMENTDETAILSToolStripMenuItem,
+            this.bILLGENERATEToolStripMenuItem,
+            this.bILLSTATUSToolStripMenuItem,
+            this.eNTRYDETAILSToolStripMenuItem,
+            this.oTHERSToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1522, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuapartmentdetails";
+            // 
+            // APARTMENTDETAILSToolStripMenuItem
+            // 
+            this.APARTMENTDETAILSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.APARTMENTDETAILSToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.APARTMENTDETAILSToolStripMenuItem.Name = "APARTMENTDETAILSToolStripMenuItem";
+            this.APARTMENTDETAILSToolStripMenuItem.Size = new System.Drawing.Size(217, 29);
+            this.APARTMENTDETAILSToolStripMenuItem.Text = "APARTMENT DETAILS";
+            // 
+            // bILLGENERATEToolStripMenuItem
+            // 
+            this.bILLGENERATEToolStripMenuItem.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.bILLGENERATEToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bILLGENERATEToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.bILLGENERATEToolStripMenuItem.Name = "bILLGENERATEToolStripMenuItem";
+            this.bILLGENERATEToolStripMenuItem.Size = new System.Drawing.Size(161, 29);
+            this.bILLGENERATEToolStripMenuItem.Text = "BILL GENERATE";
+            // 
+            // bILLSTATUSToolStripMenuItem
+            // 
+            this.bILLSTATUSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.bILLSTATUSToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.bILLSTATUSToolStripMenuItem.Name = "bILLSTATUSToolStripMenuItem";
+            this.bILLSTATUSToolStripMenuItem.Size = new System.Drawing.Size(136, 29);
+            this.bILLSTATUSToolStripMenuItem.Text = "BILL STATUS";
+            // 
+            // eNTRYDETAILSToolStripMenuItem
+            // 
+            this.eNTRYDETAILSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.eNTRYDETAILSToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.eNTRYDETAILSToolStripMenuItem.Name = "eNTRYDETAILSToolStripMenuItem";
+            this.eNTRYDETAILSToolStripMenuItem.Size = new System.Drawing.Size(163, 29);
+            this.eNTRYDETAILSToolStripMenuItem.Text = "ENTRY DETAILS";
+            // 
+            // oTHERSToolStripMenuItem
+            // 
+            this.oTHERSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mINUTEBOOKToolStripMenuItem});
+            this.oTHERSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.oTHERSToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
+            this.oTHERSToolStripMenuItem.Name = "oTHERSToolStripMenuItem";
+            this.oTHERSToolStripMenuItem.Size = new System.Drawing.Size(96, 29);
+            this.oTHERSToolStripMenuItem.Text = "OTHERS";
+            // 
+            // mINUTEBOOKToolStripMenuItem
+            // 
+            this.mINUTEBOOKToolStripMenuItem.Name = "mINUTEBOOKToolStripMenuItem";
+            this.mINUTEBOOKToolStripMenuItem.Size = new System.Drawing.Size(221, 30);
+            this.mINUTEBOOKToolStripMenuItem.Text = "MINUTE BOOK";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.btnLogout);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(1310, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(212, 284);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnLogout.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLogout.Location = new System.Drawing.Point(0, 249);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(212, 35);
+            this.btnLogout.TabIndex = 2;
+            this.btnLogout.Text = "   Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Position";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 127);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1522, 700);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "                                                                                 " +
+    "                       APARTMENT MANAGEMENT SYSTEM";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem APARTMENTDETAILSToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem bILLGENERATEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bILLSTATUSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eNTRYDETAILSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oTHERSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mINUTEBOOKToolStripMenuItem;
+    }
+}
+
